f17a10c783627f0f62817b998da5b2ecd708f2fe
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2017-08-02 00:40:30 -0400

    Initial Commit

---
+++ FriesOrNot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriesOrNot
+//
+//  Created by Sabrina Smai on 2017-08-02.
+//  Copyright © 2017 Sabrina Smai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
